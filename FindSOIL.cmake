32036adc117ce534961cfd3086c31793adefd337
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2015-11-02 10:41:05 +0100

    s

--- FindSOIL.cmake
+++ FindSOIL.cmake
@@ -1,5 +1,9 @@
+message("hoi ${SOIL_LIBRARIES}")
+
 find_path(SOIL_INCLUDE_DIRS SOIL/SOIL.h)
 find_library(SOIL_LIBRARIES NAMES SOIL)
+
+message("hai ${SOIL_LIBRARIES}")
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(SOIL DEFAULT_MSG SOIL_INCLUDE_DIRS SOIL_LIBRARIES)
