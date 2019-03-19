f222e4f167e171fe040ece5d4ffccfb8b555fc97
Alwin Esch <alwin.esch@web.de>
2019-03-19 23:38:46 +0100

    fix shaders (OSX not support gl_FragColor)

--- screensaver.matrixtrails/resources/shaders/GL/frag.glsl
+++ screensaver.matrixtrails/resources/shaders/GL/frag.glsl
@@ -5,7 +5,9 @@ uniform sampler2D u_texture;
 in vec2 v_coord;
 in vec4 v_color;
 
+out vec4 fragColor;
+
 void main ()
 {
-  gl_FragColor = texture(u_texture, v_coord) * v_color;
+  fragColor = texture(u_texture, v_coord) * v_color;
 }
